e10ee003c1cf1398cbb82765bcf7569cac274974
Jon <gravityrideseverything1@gmail.com>
2025-04-28 20:33:09 -0400

    add eslint

--- src/parser.ts
+++ src/parser.ts
@@ -2,7 +2,10 @@ import { Chord } from "./types"
 import { swapFlatsWithSharps, chordStructures, notes } from "./util";
 
 export const getChordNotesByName = (chord: Chord): string[] => {
-  let bassNote = chord.name?.endsWith('9') ? '' : chord.name?.split('/')?.[1];
+  /*
+  Given a chord, return the associated notes.
+  */
+  const bassNote = chord.name?.endsWith('9') ? '' : chord.name?.split('/')?.[1];
   if (bassNote) chord.name = chord.name?.split('/')?.[0];
 
   if (!chord.name || chord.name === '') return [];
@@ -17,13 +20,13 @@ export const getChordNotesByName = (chord: Chord): string[] => {
   let splitIdx = 0;
   if (transformedChordName.includes(' ')) splitIdx = transformedChordName.indexOf(' ');
   if (transformedChordName?.[1] === '#' || transformedChordName?.[1] === 'b') splitIdx = 1;
-  
+
   const rootNote = transformedChordName?.substring(0, splitIdx + 1)?.trim();
   const chordType = transformedChordName?.substring(splitIdx + 1)?.trim();
-  
+
   /* If no chord type found, return */
   if (!Object.keys(chordStructures).includes(chordType)) return [];
-  
+
   const rootNoteIdx: number = notes?.indexOf(rootNote);
   const chordNotes: string[] = [];
 
@@ -33,7 +36,7 @@ export const getChordNotesByName = (chord: Chord): string[] => {
     diagram up 12 half-steps.
   */
 
-    let addIdx = 0;
+  let addIdx = 0;
   if (
     notes.indexOf(bassNote) > -1
     && notes.indexOf(bassNote) > rootNoteIdx
